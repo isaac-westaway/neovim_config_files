19a4b495dafd4256eae4cb8136823d64f2630934
isaac-westaway <i.westaway3119@gmail.com>
2024-05-06 22:24:32 +1000

    Chore: Some Comments & Changes TODO: Terminal Environment Window Splitting With Hotkeys

--- lua/autocmds/nvimtree_autorun.lua
+++ lua/autocmds/nvimtree_autorun.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_create_autocmd("VimEnter", {
         -- this file is no longer used i might keep it here in case i do something with it again
+	-- boilerplate for autorun
 	callback = function()
 		vim.schedule(function()
 			print("callback working")
