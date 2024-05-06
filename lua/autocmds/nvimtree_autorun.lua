6cf127d5613560d91c9db14f26c93b41107b6900
isaac-westaway <125730392+isaac-westaway@users.noreply.github.com>
2024-05-06 22:16:14 +1000

    Update nvimtree_autorun.lua

--- lua/autocmds/nvimtree_autorun.lua
+++ lua/autocmds/nvimtree_autorun.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd("VimEnter", {
-
+        -- this file is no longer used i might keep it here in case i do something with it again
 	callback = function()
 		vim.schedule(function()
 			print("callback working")
